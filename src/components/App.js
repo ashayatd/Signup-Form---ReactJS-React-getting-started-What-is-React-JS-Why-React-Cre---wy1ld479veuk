f8dee94f0d5b1d9b4e30de1463d2dc87d3c37f5e
Ashay Tamrakar <48744162+ashayatd@users.noreply.github.com>
2022-11-04 10:59:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,76 +1,102 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, {useState} from 'react';
+import '../styles/App.css'
 
-const App = () => {
-const [getError, setError] = useState("") ;
-const [getForm, setForm] = useState({
-  name:"",
-  email:"",
-  gender:"male",
-  phoneNumber:"",
-  password:""
-});
+function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [gender, setGender] = useState('male');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [password, setPassword] = useState('');
+  const [userName, setUserName] = useState('');
+  const [nameError, setNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [phoneNumberError, setPhoneNumberError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [emptyError, setEmptyError] = useState('');
+  const [genderError, setGenderError] = useState('');
 
-const OnchangeHandler = (event)=>{
-setForm({
-  ...getForm,
-  [event.target.name]:event.target.value
-})
-}
-const OnsubmitHandler = (event)=>{
-  let errorFlag = false;
-  let flag = false;
-  event.preventDefault();
-  if(getForm.name==="" || getForm.email==="" || getForm.phoneNumber==="" || getForm.password===""){
-    setError("All fields are mandatory");
-    errorFlag = true;
+  const resetErrorDefault = () =>{
+      setNameError('');
+      setPhoneNumberError('');
+      setPasswordError('');
+      setEmailError('');
+      setEmptyError('');
+      setGenderError('');
   }
-  for(let i=0; i<getForm.email.length ;i++){
-    if(getForm.email[i]==="@"){
-      flag=true;
+  const validate = () => {
+    if(name === '' || email === '' || gender === '' || phoneNumber === '' || password === ''){
+      setEmptyError('All fields are mandatory');
+      setUserName('');
+      return false;
+    }
+    if(!name.match(/^[A-Za-z0-9- ]+$/)) {
+      setNameError('Name is not alphanumeric');
+      setUserName('');
+      return false;
+  }
+    if(!email.includes('@')){
+      setEmailError('email must contain @');
+      setUserName('');
+      return false;
+    }
+    if (!gender.match(/male|female|others/i)) {
+      setGenderError('Please identify as male, female or others');
+      setUserName('');
+      return false;
+    }
+    if(!phoneNumber.match(/^[0-9]+$/)){
+      setPhoneNumberError('Phone Number must contain only numbers');
+      setUserName('');
+      return false;
+    }
+    if(password.length < 6){
+      setPasswordError('Password must contain atleast 6 letters');
+      setUserName('');
+      return false;
+    }
+    return true;
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    resetErrorDefault();
+    const isValid = validate();
+    if(isValid) {
+      setName('');
+      setEmail('');
+      setGender('');
+      setPhoneNumber('');
+      setPassword('');
+      resetErrorDefault();
+      // console.log(name, email, gender, phoneNumber, password);
+      setUserName(email.substr(0, email.indexOf('@')));
     }
   }
-  if(flag===false){setError("Email must contain @") 
-  errorFlag=true}
-  if(getForm.gender===""){
-    setError("Please identify as male, female or others")
-    errorFlag = true;}
-  
-  if(errorFlag===false){
-    setError("")
-  }
-}
 
   return (
-    <>
-    <form>
-      <div>
-        Name: <input data-testid='name' onChange={OnchangeHandler} name="name" /> 
-      </div>
-      <div>
-        Email: <input data-testid = 'email' name="email" onChange={OnchangeHandler} />
-      </div>
-      <div>
-        <select data-testid="gender" onChange={OnchangeHandler} name="gender">
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-          <option value="others">Others</option>
-        </select>
-      </div>
-      <div>
-        Phone Number: <input data-testid = 'phoneNumber' type='number' name="phoneNumber" onChange={OnchangeHandler} />
-      </div>
-      <div>
-        Password: <input  data-testid = 'password' type='password' name="password" onChange={OnchangeHandler} />
-      </div>
-      <button data-testid = 'submit' onClick={OnsubmitHandler}>
-        Submit
-      </button><br/>
-      {getError}
-    </form>
-    </>
-  )
+    <div className="App">
+        <form onSubmit={handleSubmit}>
+          <input data-testid='name' type='text' placeholder='name' value={name} onChange={(e) => setName(e.target.value)}></input>
+          <span>{nameError}</span>
+          <input data-testid='email' type='text' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}></input>
+          <span>{emailError}</span>
+          <select data-testid='gender' name="gender" value={gender} onChange={(e) => setGender(e.target.value)}>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+          <span>{genderError}</span>
+          <input data-testid='phoneNumber' type='text' placeholder='phone number' value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}></input>
+          <span>{phoneNumberError}</span>
+          <input data-testid='password' type='password' placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)}></input>
+          <span>{passwordError}</span>
+          <span>{emptyError}</span>
+          <input type="submit" data-testid='submit' value="Submit" />
+        </form>
+        <div className='username'>
+          <h2> { (userName) ? "Hello " + (userName) : '' }</h2>
+        </div>
+    </div>
+  );
 }
-
 
 export default App;
